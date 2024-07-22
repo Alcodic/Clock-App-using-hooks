2c2b25b8c90c87f70e992b420f9b5b6b7d5cc891
Alcodic <avi211169@gmail.com>
2024-07-22 10:09:07 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import Clock from "./components/Clock";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Clock App</h1>
+        <Clock />
+      </header>
+    </div>
+  );
+}
+
+export default App;
